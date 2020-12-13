5751410bcaf0c5001622286d1991b80fa08035a0
Dmitry <gulevich.ds@phystech.edu>
2020-12-13 13:38:14 +0300

    13.12

--- back/back/api/models.py
+++ back/back/api/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Users(models.Model):
+    login = models.CharField(max_length=50)
+    email = models.EmailField()
+    password = models.CharField(max_length=300)
+
+    def __str__(self):
+        return self.login
+
+
+class Tables(models.Model):
+    GAME_TYPES = [
+        ('pref', 'Preference')
+    ]
+    
+    name = models.CharField(max_length=50, primary_key=True)
+    game = models.CharField(max_length=20, choices=GAME_TYPES)
+
+    def __str__(self):
+        return self.name
+
+class Players(models.Model):
+    table = models.ForeignKey(Tables, on_delete=models.CASCADE) # if table deleted player on it deleted to
+    user = models.ForeignKey(Users, on_delete=models.SET_NULL, blank=True, null=True) # if user deleted player becomes unauthorized
--- back/back/api/urls.py
+++ back/back/api/urls.py
@@ -13,8 +13,15 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.urls import path
+from django.urls import path, include
+from rest_framework import routers
+
+from api import views
+
+router = routers.DefaultRouter()
+router.register(r'users', views.UsersViewSet)
+router.register(r'tables', views.TablesViewSet)
 
 urlpatterns = [
-    
+    path('', include(router.urls))
 ]
